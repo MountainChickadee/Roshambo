47c8a0a9b3d78b95dda4cd196d573d3ce8f7b6e0
MountainChickadee <osmo13@gmail.com>
2025-06-19 14:45:18 -0600

    getComputerChoise function is working

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('Hello World');
+function getComputerChoice() {
+    let rand_num = Math.floor(Math.random() * 3);
+    switch (rand_num) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+}
+
+console.log(getComputerChoice())
