00d9fcd7cc3d85d346f85f8679e2abb783689384
MountainChickadee <osmo13@gmail.com>
2025-06-23 12:58:12 -0600

    Finished playRound function

--- main.js
+++ main.js
@@ -15,4 +15,28 @@ function getHumanChoice() {
     return choice.toLowerCase();
 }
 
+function playRound(humanChoice, computerChoice) {
+    
+    if (humanChoice == computerChoice) {
+        console.log(`Both players choose ${humanChoice}. Its a tie!`)
+        return 'tie';    
+    }
+    else if ((humanChoice == 'rock' && computerChoice == 'scissors') || (humanChoice == 'paper' && computerChoice == 'rock') || (humanChoice == 'scissors' && computerChoice == 'paper')) {
+        console.log(`${humanChoice.toUpperCase()} beats ${computerChoice}. Player 1 is the winner!`)
+        return 'human';
+    }
+    else {
+        console.log(`${computerChoice.toUpperCase()} beats ${humanChoice}. The computer is the winner!`)
+        return 'computer';
+    }
+
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+outcome = playRound(humanSelection, computerSelection);
 console.log(getHumanChoice())
